3e7546f3fc95792a7337e31827c5885c17671b7e
ShiChao TANG <shichao.tang@gmail.com>
2024-12-22 11:35:35 +0800

    没什么关系，只是个开始。

--- extension.js
+++ extension.js
@@ -1,36 +1,60 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-
-/**
- * @param {vscode.ExtensionContext} context
- */
 function activate(context) {
+    function updateMinimapVisibility() {
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            const config = vscode.workspace.getConfiguration('minimapToggle');
+            const minWidthForMinimap = config.get('minWidthForMinimap');
+            const editorWidth = getEditorWidth(editor);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "tsc-extension" is now active!');
+            if (editorWidth > minWidthForMinimap) {
+                vscode.workspace.getConfiguration().update('editor.minimap.enabled', true, vscode.ConfigurationTarget.Global);
+            } else {
+                vscode.workspace.getConfiguration().update('editor.minimap.enabled', false, vscode.ConfigurationTarget.Global);
+            }
+        }
+    }
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('tsc-extension.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+    // 监听窗口变化以更新minimap的显示状态
+    vscode.window.onDidChangeTextEditorViewColumn(updateMinimapVisibility);
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from tsc-extension!');
-	});
+    // 监听配置变化以更新minimap的显示状态
+    vscode.workspace.onDidChangeConfiguration(event => {
+        if (event.affectsConfiguration('minimapToggle.minWidthForMinimap')) {
+            updateMinimapVisibility();
+        }
+    });
 
-	context.subscriptions.push(disposable);
+    // 初始调用以设置minimap的显示状态
+    updateMinimapVisibility();
+
+    context.subscriptions.push(
+        vscode.workspace.onDidChangeConfiguration(event => {
+            if (event.affectsConfiguration('minimapToggle.minWidthForMinimap')) {
+                updateMinimapVisibility();
+            }
+        })
+    );
 }
 
-// This method is called when your extension is deactivated
+/**
+ * 尝试获取编辑器宽度的函数
+ * 注意：VS Code API 没有直接提供获取编辑器宽度的方法，以下方法仅供参考
+ */
+function getEditorWidth(editor) {
+    // 这里需要根据实际情况来获取编辑器的宽度，可能需要通过 DOM 操作或其他方式
+    // 以下代码仅为示例，实际中需要根据 VS Code 的版本和 API 进行调整
+    const editorElement = vscode.window.visibleTextEditors[0]?.view?.overviewRule?.clientWidth;
+    return editorElement || vscode.window.visibleTextEditors[0]?.options?.get('minimap.maxColumn');
+}
+
+/**
+ * 扩展停用时调用
+ */
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+    activate,
+    deactivate
+};
